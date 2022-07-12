ad7d85c6738aaa4f6f9ec8017380f4786915f073
JerlinJR <infodotjerlin@gmail.com>
2022-07-12 15:04:20 +0530

    signup page

---
+++ templates/_signup.php
@@ -0,0 +1,33 @@
+<main class="form-signup w-100 m-auto">
+  <form action="test.php" method="post">
+    <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Please Sign Up</h1>
+
+    <div class="form-floating">
+      <input type="text" class="form-control" name="username" placeholder="username">
+      <label for="floatingInput">Username</label>
+    </div>
+
+    <div class="form-floating">
+      <input type="text" class="form-control" name="phone" placeholder="phone">
+      <label for="floatingInput">Phone</label>
+    </div>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" name="username" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+
+    <div class="form-floating">
+      <input type="password" class="form-control" name="password" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+  </form>
+</main>
