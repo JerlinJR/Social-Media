124cb01cbea664e9301738154fe99f41b41cd715
JerlinJR <infodotjerlin@gmail.com>
2022-07-18 22:07:54 +0530

    Database Connection Using OOPS :)

--- test.php
+++ test.php
@@ -6,8 +6,13 @@ include 'libs/load.php';
 
 // print_r($_POST);
 
-signup("jerlidn", "sdaf" ,"dsaf" ,"dsafsadfds");
+signup("jerlidn", "sdaf", "dsaf", "dsafsadfds");
 
+
+
+
+// $conn = Database::getConnection();
+// $conn = Database::getConnection();
 
 
 ?>
