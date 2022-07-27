05daafc08bf01b75f243042e36084c1b0f84be9f
JerlinJR <infodotjerlin@gmail.com>
2022-07-27 09:58:52 +0000

    Validate credentials function for login

--- libs/load.php
+++ libs/load.php
@@ -9,11 +9,37 @@ function load_template($name)
 
 function validate_credentials($username, $password)
 {
-    if ($username=="jerlin@jerlin.com" && $password == "1") {
-        return true;
-    } else {
-        return false;
+    $servername = "mysql.selfmade.ninja";
+    $user_name = "Jerlin";
+    $pass_word = "7@XuGQYijiKBFWm";
+    $dbname = "Jerlin_app";
+
+    // Create connection
+    $conn = new mysqli($servername, $user_name, $pass_word, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
+    $sql = "SELECT `id`, `username`, `password`,`email` FROM `auth` WHERE username = '$username';";
+    $result = $conn->query($sql);
+
+    $error = false;
+    if ($result->num_rows > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if($row['username'] == $username and $row['password'] == $password){
+            echo 'Login sucess';
+            $error = false;
+        } else {
+            echo 'Login failed';
+            $error = true;
+        }
+
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        $error = $conn->error;
+    }
+    return $error;
+    print($error);
 }
 
 function signup($username, $password, $email, $phone)
@@ -34,12 +60,12 @@ function signup($username, $password, $email, $phone)
 VALUES ('$username', '$password', '$email', '$phone', '0', '1');";
 
     $error = false;
-    if($conn->query($sql) === true) {
+    if ($conn->query($sql) === true) {
         $error = false;
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
         $error = $conn->error;
- }
+    }
 
     $conn->close();
     return $error;
