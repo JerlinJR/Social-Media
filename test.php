0d949ca0e2a383feeb6c802c9d65e01bf7c8a34e
JerlinJR <infodotjerlin@gmail.com>
2022-07-27 09:58:52 +0000

    Validate credentials function for login

--- test.php
+++ test.php
@@ -7,12 +7,21 @@ include 'libs/load.php';
 // print_r($_POST);
 
 
-if(signup("JElasjdkfrisda", "addsafsfd", "JfdsErlin@jelkr.ds", "131111")){
-    echo "sucess";
-} else {
-    echo "fail;";
-}
+// if(signup("JElasjdkfrisda", "addsafsfd", "JfdsErlin@jelkr.ds", "131111")){
+//     echo "sucess";
+// } else {
+//     echo "fail;";
+// }
 
+$error = validate_credentials('jerlin', "jerlin");
+
+if($error){
+
+    echo "true";
+
+} else {
+    echo "false";
+}
 
 // $conn = Database::getConnection();
 // $conn = Database::getConnection();
