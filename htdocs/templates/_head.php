d09b694383656ee461d231548e3f252123671d4e
JerlinJR <infodotjerlin@gmail.com>
2022-09-03 11:00:49 +0000

    Given theBasic Structure

---
+++ htdocs/templates/_head.php
@@ -0,0 +1,34 @@
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="Jerlin" content="Social media">
+	<meta name="generator" content="Hugo 0.84.0">
+	<title>Social media</title>
+
+	<link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/album/">
+	<!-- <link rel="stylesheet" href="/css/login.css"> -->
+
+
+	<!-- Bootstrap core CSS -->
+	<link href="<?=get_config('base_path')?>assets/dist/css/bootstrap.min.css" rel="stylesheet">
+	<?if (file_exists($_SERVER["DOCUMENT_ROOT"].get_config('base_path').'css/'.basename($_SERVER['PHP_SELF'],".php").".css")) {?>
+	<link rel="stylesheet" href="<?=get_config('base_path')?>css/<?=basename($_SERVER['PHP_SELF'],".php")?>.css">
+	<? } ?>
+	<style>
+		.bd-placeholder-img {
+			font-size: 1.125rem;
+			text-anchor: middle;
+			-webkit-user-select: none;
+			-moz-user-select: none;
+			user-select: none;
+		}
+
+		@media (min-width: 768px) {
+			.bd-placeholder-img-lg {
+				font-size: 3.5rem;
+			}
+		}
+	</style>
+	
+</head>
