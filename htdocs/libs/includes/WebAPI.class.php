24de5eded67760fcef8a6888c61dbf72389671fb
JerlinJR <infodotjerlin@gmail.com>
2022-09-18 11:23:06 +0000

    working Structure

---
+++ htdocs/libs/includes/WebAPI.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class WebAPI{
+
+    public function __construct()
+    {
+        // if(php_sapi_name() == "cli"){
+        //     global $__siteConfig;
+        //     $__siteConfig = file_get_contents("/home/Jerlin/htdocs/app/htdocs/project/databaseConfig.json");
+        //     print($__siteConfig);
+        // } else if(php_sapi_name() == "apache2handler") {
+        //     global $__siteConfig;
+        //     $__siteConfig = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/../databaseConfig.json');
+
+        // }
+        $__siteConfig_path = __DIR__."../../../databaseConfig.json";
+        $__siteConfig = file_get_contents($__siteConfig_path);
+        Database::getConnection();
+    }
+
+    public function initiateSession(){
+        // Session::start();
+
+    }
+
+}
