180baa6d9baba06669a18344cfc549c5dfc0c0cf
JerlinJR <infodotjerlin@gmail.com>
2023-04-03 16:54:38 +0000

    Working Image Upload

--- htdocs/libs/app/Post.class.php
+++ htdocs/libs/app/Post.class.php
@@ -9,13 +9,14 @@ class Post{
     public $conn;
 
     public static function registerPost($text,$image_tmp){
+        // print("Is file:" .is_file($image_tmp));
         if(is_file($image_tmp) and exif_imagetype($image_tmp) !== False){
             $author = Session::getUser()->getEmail();
             $image_name = md5($author.time()) . image_type_to_extension(exif_imagetype($image_tmp));
-            $image_path = get_config("upload_path").$image_name;
+            $image_path = get_config("upload_path") . $image_name;
         if(move_uploaded_file($image_tmp,$image_path)){
             $sql = "INSERT INTO `posts` (`post_text`, `multiple_images`,`image_uri`, `like_count`, `uploaded_time`, `owner`)
-            VALUES ('$text', 0 , '/images/'.$image_name, '0', now(), '$author');";
+            VALUES ('$text', '0' , '/images/$image_name', '0', now(), '$author');";
             $conn = Database::getConnection();
             if($conn->query($sql)){
                 $id = mysqli_insert_id($conn);
